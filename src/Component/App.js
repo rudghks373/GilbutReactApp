d58e9315d5b48b29cdac56c550b5db5ee8a89321
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-04-26 16:00:20 +0900

    Updata Component
    Updata Component

---
+++ src/Component/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import IterationSample from "./IterationSample";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <IterationSample />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Component/IterationSample.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class IterationSample extends Component {
+  state = {
+    names: ["눈사람", "얼음", "눈", "바람"],
+    name: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  handleInsert = () => {
+    // names 배열에 값을 추가하고, name 값을 초기화합니다.
+    this.setState({
+      names: this.state.names.concat(this.state.name),
+      name: ""
+    });
+  };
+
+  render() {
+    const nameList = this.state.names.map((name, index) => (
+      <li key={index}>{name}</li>
+    ));
+
+    return (
+      <div>
+        <input onChange={this.handleChange} value={this.state.name} />
+        <button onClick={this.handleInsert}>추가</button>
+        <ul>{nameList}</ul>
+      </div>
+    );
+  }
+}
+
+export default IterationSample;
