153c94cb9e984efedf5ce0e9f86e43b69f0926b1
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-02 17:06:47 +0900

    Updata Delete function

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,19 +3,24 @@ import TodoItem from "../TodoItem";
 
 class TodoList extends Component {
   render() {
-    const { todos, onToggle } = this.props;
-    const todoList = todos.map(todo => (
-      <TodoItem
-        key={todo.id}
-        done={todo.done}
-        onToggle={() => onToggle(todo.id)}
-      >
-        {todo.text}
-      </TodoItem>
-    ));
-
-    return <div>{todoList}</div>;
+    const { todos, onToggle, onRemove } = this.props;
+    const todoList = todos.map(
+      todo => (
+        <TodoItem
+          key={todo.id}
+          done={todo.done}
+          onToggle={() => onToggle(todo.id)}
+          onRemove={() => onRemove(todo.id)}>
+          {todo.text}
+        </TodoItem>
+      )
+    );
+ 
+    return (
+      <div>
+        {todoList}
+      </div>
+    );
   }
 }
-
 export default TodoList;
