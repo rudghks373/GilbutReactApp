6f07a866d23364c0bc0a7ca6c5263da296a36460
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-02 16:58:58 +0900

    Updata handleToggle Function

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,11 @@ class App extends Component {
       { id: 1, text: "컴포넌트 스타일링 해보기", done: false }
     ]
   };
+  // 일정 데이터 안에 들어가는 id 값
+  id = 1;
+  getId = () => {
+    return ++this.id; // 현재 값에서 1을 더한 값을 반환
+  };
 
   handleChange = e => {
     const { value } = e.target;
@@ -20,14 +25,58 @@ class App extends Component {
     });
   };
 
+  // 새 데이터 추가
+  handleInsert = () => {
+    const { todos, input } = this.state;
+
+    // 새 데이터 객체 만들기
+    const newTodo = {
+      text: input,
+      done: false,
+      id: this.getId()
+    };
+
+    // 배열 안에 새 데이터를 집어넣습니다.
+    this.setState({
+      todos: [...todos, newTodo],
+      input: ""
+    });
+  };
+
+  handleToggle = id => {
+    // id로 배열의 인덱스를 찾습니다.
+    const { todos } = this.state;
+    const index = todos.findIndex(todo => todo.id === id);
+
+    // 찾은 데이터의 done 값을 반전시킵니다.
+    const toggled = {
+      ...todos[index],
+      done: !todos[index].done
+    };
+
+    // slice를 사용하여 우리가 찾은 index 전후의 데이터들을 복사합니다.
+    // 그리고 그 사이에는 변경된 to do 객체를 넣어 줍니다.
+    this.setState({
+      todos: [
+        ...todos.slice(0, index),
+        toggled,
+        ...todos.slice(index + 1, todos.length)
+      ]
+    });
+  };
+
   render() {
     const { input, todos } = this.state;
-    const { handleChange } = this;
+    const { handleChange, handleInsert, handleToggle } = this;
 
     return (
       <PageTemplate>
-        <TodoInput onChange={handleChange} value={input} />
-        <TodoList todos={todos} />
+        <TodoInput
+          onChange={handleChange}
+          onInsert={handleInsert}
+          value={input}
+        />
+        <TodoList todos={todos} onToggle={handleToggle} />
       </PageTemplate>
     );
   }
