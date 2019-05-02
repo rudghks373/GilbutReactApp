819db2eb35dfadb844eb47999b0f4f6c7eecf316
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-02 16:19:28 +0900

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,11 @@ class App extends Component {
       { id: 1, text: "컴포넌트 스타일링 해보기", done: false }
     ]
   };
+  // 일정 데이터 안에 들어가는 id 값
+  id = 1;
+  getId = () => {
+    return ++this.id; // 현재 값에서 1을 더한 값을 반환
+  };
 
   handleChange = e => {
     const { value } = e.target;
@@ -20,13 +25,35 @@ class App extends Component {
     });
   };
 
+  // 새 데이터 추가
+  handleInsert = () => {
+    const { todos, input } = this.state;
+
+    // 새 데이터 객체 만들기
+    const newTodo = {
+      text: input,
+      done: false,
+      id: this.getId()
+    };
+
+    // 배열 안에 새 데이터를 집어넣습니다.
+    this.setState({
+      todos: [...todos, newTodo],
+      input: ""
+    });
+  };
+
   render() {
     const { input, todos } = this.state;
-    const { handleChange } = this;
+    const { handleChange, handleInsert } = this;
 
     return (
       <PageTemplate>
-        <TodoInput onChange={handleChange} value={input} />
+        <TodoInput
+          onChange={handleChange}
+          onInsert={handleInsert}
+          value={input}
+        />
         <TodoList todos={todos} />
       </PageTemplate>
     );
