f11fa4b9ad4c0cb207600e2a5a27eaa18438ad8d
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-02 16:11:58 +0900

    Updata Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from "react";
 import PageTemplate from "./PageTemplate";
 import TodoInput from "./TodoInput";
-
 import TodoList from "./TodoList";
 
-
-
 class App extends Component {
+  state = {
+    input: "", // input 값
+    // 일정 데이터 초깃값
+    todos: [
+      { id: 0, text: "리액트 공부하기", done: true },
+      { id: 1, text: "컴포넌트 스타일링 해보기", done: false }
+    ]
+  };
+
+  handleChange = e => {
+    const { value } = e.target;
+    this.setState({
+      input: value
+    });
+  };
+
   render() {
+    const { input, todos } = this.state;
+    const { handleChange } = this;
+
     return (
       <PageTemplate>
-        <TodoInput />
-
-        <TodoList />
-
+        <TodoInput onChange={handleChange} value={input} />
+        <TodoList todos={todos} />
       </PageTemplate>
     );
   }
